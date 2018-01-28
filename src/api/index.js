972e4bde54a1adfc154f1bf100b7102170a51c88
Tim Johnson <tjohn92@gmail.com>
2018-01-28 12:06:30 -0500

    removed unneccessary modules and broke out scheduler

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,31 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
-import fetch from 'node-fetch';
+import GetSchedule from '../service/getSchedule';
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
-
-	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
 	});
 
-	api.get('/schedule', (req, resp) => {
-		console.log("making the request");
-		fetch(`http://wirelesscats.ridetransit.org/BusSchedules/Search/Schedule?routeCode=6199&routeDirection=${req.query.direction}&bookingId=70&routeType=${req.query.routeDay}`)
-			.then( res => res.json())
-			.then( json => {
-				resp.send(json);
-			})
-			.catch(err => console.log(err));
+	api.get('/schedule', async (req, resp) => {
+
+		let response1 = GetSchedule("Inbound", "Weekday");
+		let response2 = GetSchedule("Outbound", "Weekday");
+		let response3 = GetSchedule("Inbound", "Saturday");
+		let response4 = GetSchedule("Outbound", "Saturday");
+		let response5 = GetSchedule("Inbound", "Sunday");
+		let response6 = GetSchedule("Outbound", "Sunday");
+		let finalResponse = [
+			await response1,
+			await response2,
+			await response3,
+			await response4,
+			await response5,
+			await response6,
+		];
+		resp.send(finalResponse);
 	});
 
 	return api;
---
+++ src/service/getSchedule.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+import moment from 'moment';
+
+export default (direction, routeDay) => {
+    let response = fetch(`http://wirelesscats.ridetransit.org/BusSchedules/Search/Schedule?routeCode=6199&routeDirection=${direction}&bookingId=70&routeType=${routeDay}`)
+        .then( res => res.json())
+        .then( json => {
+            let formattedResponse = {};
+            json.stationStops.map((stationStops, i) => {
+
+                const formattedStops = stationStops.map((stop,i) => {
+                    let formattedStop = moment(stop, 'hh:mm A').format('HH:mm');
+                    return formattedStop;					
+                });
+
+                let midnight = moment('00:00', 'HH:mm');
+                let firstTrains = moment('05:00', 'HH:mm');
+
+                let lateNightStops = formattedStops.filter(s => {
+                    let momentStop = moment(s, 'HH:mm');
+
+                    if (momentStop.isBetween(midnight, firstTrains) || momentStop.isSame(midnight)) {
+                        return momentStop;
+                    }
+                });
+
+                let daytimeStops = formattedStops.filter(s => {
+                    let momentStop = moment(s, 'HH:mm');
+
+                    if (!momentStop.isBetween(midnight, firstTrains) && !momentStop.isSame(midnight)) {
+                        return momentStop;
+                    }
+                });
+
+                formattedResponse[`station-${i+1}`] = [...daytimeStops, ...lateNightStops];
+            });
+            let respObject = {}
+            let identifier = direction.toLowerCase() + routeDay;
+            respObject[`${identifier}`] = formattedResponse
+            return respObject;
+        })
+        .catch(err => console.log(err));
+    return response;
+}
